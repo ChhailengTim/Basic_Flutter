a690c7fea090ca864c7110c65a63b8149bd76c03
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-02-02 14:25:44 +0700

    Scaffold text

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container(
-    color: Colors.red,
-  ));
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SETEC Studdent Application',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('RUPP Student Application'),
+        ),
+      ),
+    );
+    throw UnimplementedError();
+  }
 }
