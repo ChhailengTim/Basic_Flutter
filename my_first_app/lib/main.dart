75ba510c9a1de88b65963427770408e67551687f
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-02-03 14:08:51 +0700

    floatingActionButton

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -10,17 +10,26 @@ class myApp extends StatelessWidget {
     return MaterialApp(
       title: 'SETEC Studdent Application',
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('RUPP Student Application'),
-            backgroundColor: Colors.green,
+        appBar: AppBar(
+          title: const Text('RUPP Student Application'),
+          backgroundColor: Colors.green,
+        ),
+        body: const Center(
+          child: Text(
+            "This is no schedule.",
+            style: TextStyle(
+                fontSize: 50, color: Colors.blue, fontFamily: 'Verdana'),
           ),
-          body: const Center(
-            child: Text(
-              "This is no schedule.",
-              style: TextStyle(
-                  fontSize: 50, color: Colors.blue, fontFamily: 'Verdana'),
-            ),
-          )),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            // ignore: avoid_print
+            print('This is floatingbutton');
+          },
+          backgroundColor: Colors.blueGrey,
+        ),
+      ),
     );
     //throw UnimplementedError();
   }
