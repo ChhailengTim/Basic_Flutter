9482cc95ad183ad74b00c3492aad51a149a8e28f
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-02-07 14:55:45 +0700

    AppBar | Scafflold Widget

--- appbar_scaffold/lib/main.dart
+++ appbar_scaffold/lib/main.dart
@@ -17,24 +17,21 @@ class MyApp extends StatelessWidget {
             icon: const Icon(Icons.home),
             onPressed: () {},
           ),
-          title: const Text("Hello Flutter App"),
+          title: const Text("Hello Flutter App CLT"),
           actions: [
             IconButton(
               onPressed: () {},
               icon: const Icon(Icons.settings),
             ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.camera_alt),
-            )
+            IconButton(onPressed: () {}, icon: const Icon(Icons.camera_alt))
           ],
           bottom: const PreferredSize(
             preferredSize: Size.fromHeight(20.0),
             child: Padding(
-              padding: EdgeInsets.all(8.0),
+              padding: EdgeInsets.all(9.0),
               child: Text(
-                "Well, it is cool, right?",
-                style: TextStyle(color: Colors.yellow),
+                "Proud of my self!",
+                style: TextStyle(color: Colors.white),
               ),
             ),
           ),
