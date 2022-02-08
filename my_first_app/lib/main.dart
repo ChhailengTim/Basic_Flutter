38ab96ce256ffa640db12e1d96f7d42d64af2480
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-02-08 15:06:20 +0700

    Update main.dart

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -11,32 +11,58 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-          alignment: Alignment.center,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                    alignment: Alignment.center,
-                    child: Image.network("http://bit.ly/2IGluzb")),
-                const Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    "Contact: 012 33 25 38",
-                    style: TextStyle(fontSize: 20.0, color: Colors.red),
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
-                  child: Text(
-                    "https://www.youtube.com",
-                    style: TextStyle(fontSize: 20.0, color: Colors.blue),
-                  ),
-                ),
-              ],
+        appBar: AppBar(
+          backgroundColor: Colors.pink,
+          leading: IconButton(
+            icon: const Icon(Icons.home),
+            onPressed: () {},
+          ),
+          title: const Text("Hello Flutter App"),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+          ],
+          bottom: const PreferredSize(
+            preferredSize: Size.fromHeight(20.0),
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "Is it cool, Right?",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
           ),
         ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(child: Image.network("http://bit.ly/2IGluzb")),
+              const ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Home"),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+              const ListTile(
+                leading: Icon(Icons.insert_emoticon),
+                title: Text("About us"),
+                trailing: Icon(Icons.arrow_forward_ios),
+              )
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.home),
+            ),
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.play_circle_filled)),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.menu))
+          ],
+        )),
       ),
     );
   }
