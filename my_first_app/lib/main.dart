db78ad118c0c6000197b09b6fec349df0c4658e1
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-02-08 23:07:21 +0700

    Update main.dart

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -12,80 +12,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.pink,
-          leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.home),
-          ),
-          title: const Text("Hello Flutter App"),
-          actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.search))
-          ],
-          bottom: const PreferredSize(
-            preferredSize: Size.fromHeight(20.0),
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                "It is cool, Right?",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-          ),
+          title: const Text("Hello App Row&Column"),
         ),
         body: Container(
-          alignment: Alignment.center,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  child: Image.network("http://bit.ly/2IGluzb"),
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    "Contact: 012 332 538",
-                    style: TextStyle(color: Colors.red, fontSize: 20.0),
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
-                  child: Text(
-                    "http://www.fb.com",
-                    style: TextStyle(fontSize: 20.0, color: Colors.blue),
-                  ),
-                )
-              ],
+          height: 120.0,
+          width: 120.0,
+          color: Colors.blue[50],
+          child: const Align(
+            alignment: Alignment.center,
+            child: FlutterLogo(
+              size: 60,
             ),
-          ),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(child: Image.network("http://bit.ly/2IGluzb")),
-              const ListTile(
-                leading: Icon(Icons.home),
-                title: Text("Home"),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-              const ListTile(
-                leading: Icon(Icons.emoji_emotions),
-                title: Text("About us"),
-                trailing: Icon(Icons.arrow_forward_ios),
-              )
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              IconButton(onPressed: () {}, icon: const Icon(Icons.home)),
-              IconButton(
-                  onPressed: () {}, icon: const Icon(Icons.play_circle_filled)),
-              IconButton(onPressed: () {}, icon: const Icon(Icons.menu))
-            ],
           ),
         ),
       ),
