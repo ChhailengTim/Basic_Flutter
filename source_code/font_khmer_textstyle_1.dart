14521226e373e4beea0a9f593c2ab07724d305d2
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-02-14 15:24:50 +0700

    Create font_khmer_textstyle_1.dart

---
+++ source_code/font_khmer_textstyle_1.dart
@@ -0,0 +1,19 @@
+return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('សួស្តី!'),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: const Text(
+            "ភាសាខ្មែរ",
+            style: TextStyle(
+                fontSize: 50.0,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                decoration: TextDecoration.underline),
+          ),
+        ),
+      ),
+    );
