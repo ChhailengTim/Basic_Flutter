51b5e9da1bccb39213a966318b7bd2664ae5a3c6
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-15 20:40:36 +0700

    Update main.dart

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -7,14 +7,33 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  get child => null;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'អក្សរខ្មែរ',
-            style: TextStyle(fontFamily: "Khmer Moul", color: Colors.black),
+        body: Container(
+          width: 200.0,
+          height: 200.0,
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.pink,
+                Colors.red,
+                Colors.orange,
+              ],
+            ),
+            color: Colors.deepPurple,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.red.withOpacity(0.5),
+                  offset: const Offset(15.0, 15.0),
+                  blurRadius: 10.0),
+            ],
           ),
         ),
       ),
