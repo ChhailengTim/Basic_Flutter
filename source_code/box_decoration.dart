c97f912c0ccd24df6b4daa1bd98444db9a18db28
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-15 23:22:30 +0700

    Update box_decoration.dart

--- source_code/box_decoration.dart
+++ source_code/box_decoration.dart
@@ -1,24 +1,31 @@
-Container(
-      width: 200.0,
-      height: 200.0,
-      decoration: BoxDecoration(
-        gradient: const LinearGradient(
-          begin: Alignment.center,
-          end: Alignment.bottomCenter,
-          colors: [
-            Colors.pink,
-            Colors.red,
-            Colors.orange,
-          ],
-        ),
-        color: Colors.deepPurple,
-        shape: BoxShape.circle,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.red.withOpacity(0.5),
-            offset: const Offset(15.0, 15.0),
-            blurRadius: 10.0,
+MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Sad! Day perk")),
+        body: Container(
+          alignment: Alignment.center,
+          child: Container(
+            width: 200.0,
+            height: 200.0,
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.pink,
+                  Colors.red,
+                  Colors.orange,
+                ],
+              ),
+              color: Colors.deepPurple,
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.red.withOpacity(0.5),
+                    offset: Offset(15.0, 15.0),
+                    blurRadius: 10.0),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
