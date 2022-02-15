3757da1a5f7635cef912bbc479d8e56697df5c2a
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-15 20:40:24 +0700

    Create padding.dart

---
+++ source_code/padding.dart
@@ -0,0 +1,29 @@
+MaterialApp(
+      home: Scaffold(
+        body: Container(
+          alignment: Alignment.center,
+          color: const Color.fromRGBO(66, 155, 243, 1.0),
+          child: const Text(
+            "Hello Flutter",
+            style: TextStyle(color: Colors.red, fontSize: 32.0),
+          ),
+          //padding: const EdgeInsets.all(40.0),
+          /* padding: const EdgeInsets.symmetric(
+            vertical: 10.0,
+            horizontal: 50.0,
+          ), */
+          /* padding: const EdgeInsets.fromLTRB(
+            20.0,
+            30.0,
+            40.0,
+            50.0,
+          ), */
+          /* padding: const EdgeInsets.only(
+            left: 20.0,
+            bottom: 40.0,
+            right: 40.0,
+            top: 50.0,
+          ), */
+        ),
+      ),
+    );
