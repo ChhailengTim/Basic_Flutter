76801e36a344adb24a2c2ea15bc01a76574318bf
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-15 18:53:17 +0700

    Create child_protertie.dart

---
+++ source_code/child_protertie.dart
@@ -0,0 +1,12 @@
+MaterialApp(
+      home: Scaffold(
+        body: Container(
+          alignment: Alignment.center,
+          color: const Color.fromRGBO(66, 155, 243, 1.0),
+          child: const Text(
+            "Hello Flutter",
+            style: TextStyle(color: Colors.red, fontSize: 32.0),
+          ),
+        ),
+      ),
+    );
