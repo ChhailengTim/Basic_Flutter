f1fcff68d3290ceeeecc8dabadd9b8d67c6dd757
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-15 22:39:26 +0700

    Create box_decoration.dart

---
+++ source_code/box_decoration.dart
@@ -0,0 +1,24 @@
+Container(
+      width: 200.0,
+      height: 200.0,
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          begin: Alignment.center,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.pink,
+            Colors.red,
+            Colors.orange,
+          ],
+        ),
+        color: Colors.deepPurple,
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.red.withOpacity(0.5),
+            offset: const Offset(15.0, 15.0),
+            blurRadius: 10.0,
+          ),
+        ],
+      ),
+    );
