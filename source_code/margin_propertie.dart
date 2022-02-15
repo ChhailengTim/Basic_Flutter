222084a6b0d96e3da6ad9b48ea110d275a9a9978
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-15 19:04:42 +0700

    Create margin_propertie.dart

---
+++ source_code/margin_propertie.dart
@@ -0,0 +1,23 @@
+MaterialApp(
+      home: Scaffold(
+        body: Container(
+          alignment: Alignment.center,
+          color: const Color.fromRGBO(66, 155, 243, 1.0),
+          child: const Text(
+            "Hello Flutter",
+            style: TextStyle(color: Colors.red, fontSize: 32.0),
+          ),
+          //margin: const EdgeInsets.all(15.0),
+          margin: const EdgeInsets.symmetric(
+            vertical: 10.0,
+            horizontal: 50.0,
+          ),
+margin: const EdgeInsets.fromLTRB(
+            20.0,
+            30.0,
+            40.0,
+            50.0,
+          ),
+        ),
+      ),
+    );
