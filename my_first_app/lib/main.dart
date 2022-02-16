8399c97b9198990a6b5e6d36ae0909cd587d1d3e
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-16 22:40:14 +0700

    Update main.dart

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -13,11 +13,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-        title: const Text("Hello"),
-      )),
+        appBar: AppBar(
+          title: const Text("Hello"),
+        ),
+        body: Column(
+          children: const [
+            Icon(
+              Icons.camera,
+              size: 200,
+            ),
+            Icon(
+              Icons.account_box,
+              size: 230.0,
+            ),
+            Icon(
+              Icons.place,
+              size: 200.0,
+            ),
+            Icon(
+              Icons.info,
+              size: 250.0,
+            ),
+          ],
+        ),
+      ),
     );
   }
-
-  void setState(Null Function() param0) {}
 }
