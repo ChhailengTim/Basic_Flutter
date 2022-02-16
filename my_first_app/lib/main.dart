9fd1905d233bbc1cf7f1c68df5be45e02df5b588
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-16 23:09:02 +0700

    Update main.dart

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -16,25 +16,29 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Hello"),
         ),
-        body: Column(
-          children: const [
-            Icon(
-              Icons.camera,
-              size: 200,
-            ),
-            Icon(
-              Icons.account_box,
-              size: 230.0,
-            ),
-            Icon(
-              Icons.place,
-              size: 200.0,
-            ),
-            Icon(
-              Icons.info,
-              size: 250.0,
-            ),
-          ],
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: const [
+              Icon(
+                Icons.camera,
+                size: 200,
+              ),
+              Icon(
+                Icons.account_box,
+                size: 230.0,
+              ),
+              Icon(
+                Icons.place,
+                size: 200.0,
+              ),
+              Icon(
+                Icons.info,
+                size: 250.0,
+              ),
+            ],
+          ),
         ),
       ),
     );
