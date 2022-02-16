5030ffbd16fe2140fecd8e589427ea6b23b291b0
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-16 21:20:42 +0700

    Update main.dart

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -13,26 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text("Keybord Input")),
-        body: Container(
-          padding: EdgeInsets.only(left: 20.0, right: 20.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20.0),
-            color: Colors.pink.withOpacity(0.5),
-          ),
-          child: TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              labelStyle: TextStyle(fontSize: 15.0, color: Colors.white),
-              hintText: "Enter your email",
-              hintStyle: TextStyle(fontSize: 15.0, color: Colors.white54),
-              labelText: "Email",
-              border: InputBorder.none,
-            ),
-            obscureText: false,
-          ),
-        ),
-      ),
+          appBar: AppBar(
+        title: const Text("Hello"),
+      )),
     );
   }
+
+  void setState(Null Function() param0) {}
 }
