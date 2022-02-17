d9d539ec2b39a773fcc9f21597ffc72768a8035c
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-17 22:01:26 +0700

    Create list_tile.dart

---
+++ source_code/list_tile.dart
@@ -0,0 +1,16 @@
+ListView(
+          children: const [
+            ListTile(
+              leading: Icon(Icons.wb_sunny),
+              title: Text("Sunny"),
+              subtitle: Text("The sun is shinning"),
+              trailing: Icon(Icons.keyboard_arrow_right),
+            ),
+            ListTile(
+              leading: Icon(Icons.wb_cloudy),
+              title: Text("Cloudy"),
+              subtitle: Text("The sky is cloudy"),
+              trailing: Icon(Icons.keyboard_arrow_right),
+            ),
+          ],
+        ),
