60579bf776367ae56ac263334cef112748a8c987
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-02-19 14:40:05 +0700

    Create grid_view_builder.dart

---
+++ source_code/grid_view_builder.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  get child => null;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("ListView Example"),
+        ),
+        body: GridView.builder(
+          itemCount: 10,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 20,
+          ),
+          itemBuilder: (context, index) {
+            return Container(
+              alignment: Alignment.center,
+              color: Colors.pink[100 * (index % 9)],
+              child: Text("gride item $index"),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
