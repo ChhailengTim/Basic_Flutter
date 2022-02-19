49569ff476bb4e7133f0342f3d008ddfbc05ecb5
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-02-19 10:34:55 +0700

    Update main.dart

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -25,37 +25,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("ListView Example"),
-        ),
-        body: ListView.builder(
+          appBar: AppBar(
+            title: const Text("ListView Example"),
+          ),
+          body: ListView.builder(
             itemCount: fruits.length,
             itemBuilder: (context, index) {
-              return Container(
-                margin: const EdgeInsets.only(bottom: 10.0),
-                color: Colors.white,
-                child: Row(
-                  children: [
-                    Container(
-                      width: 100.0,
-                      height: 100.0,
-                      child: Image.asset(
-                        fruits[index].imageUrl!,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text(
-                        fruits[index].title!,
-                        style: const TextStyle(fontSize: 22.0),
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            }),
-      ),
+              if (index % 2 == 0) {
+                return Container(
+                  color: Colors.yellow,
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    fruits[index].title!,
+                    style: const TextStyle(fontSize: 25.0),
+                  ),
+                );
+              } else {
+                return Container(
+                  color: Colors.lightBlue,
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    fruits[index].title!,
+                    style: const TextStyle(fontSize: 25.0),
+                  ),
+                );
+              }
+            },
+          )),
     );
   }
 }
