41b94ac5864c847e0ea994868ab8f42f4d59c3e7
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-02-22 11:46:19 +0700

    Create sliver_appbar_example.dart

---
+++ source_code/sliver_appbar_example.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// ignore: must_be_immutable
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              leading: const Icon(Icons.face),
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.settings),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.person),
+                ),
+              ],
+              pinned: false,
+              title: const Text("Sliver AppBar Demo"),
+              floating: true,
+              flexibleSpace: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Colors.pink,
+                      Colors.cyan,
+                    ],
+                  ),
+                ),
+              ),
+              expandedHeight: 80,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void setState(Null Function() param0) {}
+}
