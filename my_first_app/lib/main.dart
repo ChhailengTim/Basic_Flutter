ac8a34b4bde825967eb889b0078edc857362d426
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-02-22 23:48:29 +0700

    Update main.dart

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,68 +1,66 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 // ignore: must_be_immutable
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  PageController pageController = PageController(
-    initialPage: 0,
-  );
-  int pageChange = 0;
-  final _listWidget = [
-    Image.network(
-        "https://nie.res.netease.com/r/pic/20190527/3abb27b4-0506-4961-981c-94b029157ce2.jpg"),
-    Image.network(
-        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/marvel-cinematic-universe-order-1601976266.jpg"),
-    Image.network(
-        "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/DA2E198288BFCA56AB53340211B38DE7134E40E4521EDCAFE6FFB8CD69250DE9/scale?width=2880&aspectRatio=1.78&format=jpeg"),
-  ];
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Page View Demo"),
-          actions: [
-            IconButton(
-              onPressed: () {
-                if (pageChange >= 1) {
-                  debugPrint(pageChange.toString());
-                  pageController.animateToPage(pageChange -= 1,
-                      duration: const Duration(microseconds: 250),
-                      curve: Curves.bounceInOut);
-                }
-              },
-              icon: const Icon(Icons.arrow_back_ios),
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              leading: const Icon(Icons.face),
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.search),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.people),
+                ),
+              ],
+              title: const Text("Sliver AppBar Example"),
+              pinned: false,
+              floating: true,
+              flexibleSpace: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Colors.pink,
+                      Colors.indigo,
+                    ],
+                  ),
+                ),
+              ),
+              expandedHeight: 80,
             ),
-            IconButton(
-              onPressed: () {
-                if (pageChange < _listWidget.length - 1) {
-                  pageController.animateToPage(pageChange += 1,
-                      duration: const Duration(milliseconds: 250),
-                      curve: Curves.bounceInOut);
-                }
-              },
-              icon: const Icon(Icons.arrow_forward_ios),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return Card(
+                    child: ListTile(
+                      leading: const Icon(Icons.face),
+                      title: Text("Item #$index"),
+                      subtitle: Text("Awsome item $index"),
+                      trailing: const Icon(Icons.navigate_next),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
-        ),
-        body: PageView(
-          pageSnapping: true,
-          controller: pageController,
-          onPageChanged: (index) {
-            pageChange = index;
-          },
-          children: _listWidget,
         ),
       ),
     );
   }
-
-  void setState(Null Function() param0) {}
 }
