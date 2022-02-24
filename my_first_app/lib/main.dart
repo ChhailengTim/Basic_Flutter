96e16d2868bada8bde8f95f8ae8d0397f60bc948
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-24 22:56:38 +0700

    Update main.dart

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -4,7 +4,6 @@ void main() {
   runApp(const MyApp());
 }
 
-// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -13,54 +12,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              title: const Text("SliverAppBar Example"),
-              pinned: false,
-              floating: true,
-              flexibleSpace: Container(color: Colors.pink),
-              expandedHeight: 80,
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
             ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  return Card(
-                    child: ListTile(
-                      leading: const Icon(Icons.face),
-                      title: Text("Items #$index"),
-                      subtitle: Text("Awesome item $index"),
-                      trailing: const Icon(Icons.arrow_forward_ios),
-                    ),
-                  );
-                },
-                childCount: 10,
-              ),
-            ),
-            SliverGrid(
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  return Container(
-                    alignment: Alignment.center,
-                    color: Colors.pink[100 * (index % 9)],
-                    child: Text("grid item $index"),
-                  );
-                },
-                childCount: 20,
-              ),
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200,
-                mainAxisSpacing: 10.0,
-                crossAxisSpacing: 10.0,
-                childAspectRatio: 4.0,
-              ),
-            ),
-            SliverAppBar(
-              title: const Text("SliverAppBar at the buttom"),
-              pinned: false,
-              floating: true,
-              flexibleSpace: Container(color: Colors.indigo),
-              expandedHeight: 80,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.menu),
+              label: ("Menu"),
             ),
           ],
         ),
