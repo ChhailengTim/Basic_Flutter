e73b1b80f64a9962fc13b24ddd4a04ff55066dd5
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-25 00:00:25 +0700

    Update main.dart

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -12,18 +12,78 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.menu),
-              label: ("Menu"),
-            ),
+        bottomNavigationBar: Dashboard(),
+      ),
+    );
+  }
+}
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            if (_selectedIndex == 0)
+              // you can call custom widget here
+              Column(
+                children: const [
+                  Text("0"),
+                ],
+              )
+            else if (_selectedIndex == 1)
+              Column(
+                children: const [
+                  Text("1"),
+                ],
+              )
+            else
+              Column(
+                children: const [
+                  Text("2"),
+                ],
+              ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.headphones),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
       ),
     );
   }
