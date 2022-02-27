a5d4b7eb4570592dfef5c82f17e84ee0630239ee
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-27 19:07:52 +0700

    Create codeview.dart

---
+++ my_first_app/lib/codeview.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          children: [
+            ///box1
+            Expanded(
+              flex: 4,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              color: Colors.green,
+                              child:
+                                  const Center(child: Icon(Icons.app_blocking)),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Expanded(
+                            child: Container(
+                              color: Colors.orange,
+                              child: const Center(
+                                  child: Icon(Icons.sentiment_neutral)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            color: Colors.blueAccent,
+                            child:
+                                const Center(child: Icon(Icons.app_blocking)),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Expanded(
+                          flex: 3,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  color: Colors.white,
+                                  child: Column(
+                                    children: [
+                                      Expanded(
+                                        flex: 3,
+                                        child: Container(
+                                          color: Colors.yellow,
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        height: 10,
+                                      ),
+                                      Expanded(
+                                        flex: 1,
+                                        child: Container(
+                                          color: Colors.orange[900],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Expanded(
+                                child: Column(
+                                  children: [
+                                    Expanded(
+                                      flex: 1,
+                                      child: Container(
+                                        color: Colors.brown,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Expanded(
+                                      flex: 3,
+                                      child: Container(
+                                        color: Colors.indigo[900],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+
+            ///box2
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.blue,
+                child: const Center(child: Icon(Icons.app_blocking)),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+
+            ///box3
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.yellow,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
