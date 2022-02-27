76f9b57a987ae6cc779db52fad14640e63191a14
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-27 19:08:09 +0700

    Update main.dart

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -12,139 +12,98 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
+        appBar: AppBar(
+          leading: const Icon(Icons.list),
+          title: const Text(
+            "ចំរៀងខ្មែរ",
+            style: TextStyle(fontFamily: 'Khmer Moul'),
+          ),
+        ),
+        body: ListView(
           children: [
-            ///box1
-            Expanded(
-              flex: 4,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      color: Colors.white,
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: Container(
-                              color: Colors.green,
-                              child:
-                                  const Center(child: Icon(Icons.app_blocking)),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Expanded(
-                            child: Container(
-                              color: Colors.orange,
-                              child: const Center(
-                                  child: Icon(Icons.sentiment_neutral)),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            color: Colors.blueAccent,
-                            child:
-                                const Center(child: Icon(Icons.app_blocking)),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Expanded(
-                          flex: 3,
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Container(
-                                  color: Colors.white,
-                                  child: Column(
-                                    children: [
-                                      Expanded(
-                                        flex: 3,
-                                        child: Container(
-                                          color: Colors.yellow,
-                                        ),
-                                      ),
-                                      const SizedBox(
-                                        height: 10,
-                                      ),
-                                      Expanded(
-                                        flex: 1,
-                                        child: Container(
-                                          color: Colors.orange[900],
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Expanded(
-                                      flex: 1,
-                                      child: Container(
-                                        color: Colors.brown,
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      height: 10,
-                                    ),
-                                    Expanded(
-                                      flex: 3,
-                                      child: Container(
-                                        color: Colors.indigo[900],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+            ListTile(
+              leading: Image.network(
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl2YGEvL51tXwaKZQxrYxw9X5mwQBb08SJmg&usqp=CAU"),
+              title: const Text("Sok Pisey"),
+              subtitle: const Text("New Album 2022"),
+              trailing: const Icon(Icons.more_vert),
             ),
-            const SizedBox(
-              height: 10,
+            ListTile(
+              leading: Image.network(
+                  "https://www.khmersongs.net/data/upload/1538474073.jpg"),
+              title: const Text("So Nita"),
+              subtitle: const Text("New Album 2022"),
+              trailing: const Icon(Icons.more_vert),
             ),
-
-            ///box2
-            Expanded(
-              flex: 1,
-              child: Container(
-                color: Colors.blue,
-                child: const Center(child: Icon(Icons.app_blocking)),
-              ),
+            ListTile(
+              leading: Image.network(
+                  "https://www.khmersongs.net/data/upload/1542786926.jpg"),
+              title: const Text("So Lika"),
+              subtitle: const Text("New Album 2022"),
+              trailing: const Icon(Icons.more_vert),
             ),
-            const SizedBox(
-              height: 10,
+            ListTile(
+              leading: Image.network(
+                  "https://www.khmersongs.net/data/upload/1539266167.jpg"),
+              title: const Text("Mo Lita"),
+              subtitle: const Text("New Album 2022"),
+              trailing: const Icon(Icons.more_vert),
             ),
-
-            ///box3
-            Expanded(
-              flex: 1,
-              child: Container(
-                color: Colors.yellow,
-              ),
+            ListTile(
+              leading: Image.network(
+                  "https://www.khmersongs.net/data/upload/1539872331.jpg"),
+              title: const Text("Mo Nasa"),
+              subtitle: const Text("New Album 2022"),
+              trailing: const Icon(Icons.more_vert),
+            ),
+            ListTile(
+              leading: Image.network(
+                  "https://www.khmersongs.net/data/upload/1537544095-1584761773-.jpg"),
+              title: const Text("Ka Vannary"),
+              subtitle: const Text("New Album 2022"),
+              trailing: const Icon(Icons.more_vert),
+            ),
+            ListTile(
+              leading: Image.network(
+                  "https://www.khmersongs.net/data/upload/1537604668.jpg"),
+              title: const Text("Na Sovany"),
+              subtitle: const Text("New Album 2022"),
+              trailing: const Icon(Icons.more_vert),
+            ),
+            ListTile(
+              leading: Image.network(
+                  "https://yt3.ggpht.com/ytc/AKedOLRViaxCXTDeduJ5r8JFtKTZpK6lJ-zd1-nfgZ1k=s176-c-k-c0x00ffffff-no-rj"),
+              title: const Text("Kim Yada"),
+              subtitle: const Text("New Album 2022"),
+              trailing: const Icon(Icons.more_vert),
+            ),
+            ListTile(
+              leading: Image.network(
+                  "https://www.khmersongs.net/data/upload/1537432404.jpg"),
+              title: const Text("So Jin"),
+              subtitle: const Text("New Album 2022"),
+              trailing: const Icon(Icons.more_vert),
+            ),
+            ListTile(
+              leading: Image.network(
+                  "https://i.ytimg.com/an/z03dDPGSAUrsQ98oFMPt6Q/featured_channel.jpg?v=617f048e"),
+              title: const Text("Ah Momo"),
+              subtitle: const Text("New Album 2022"),
+              trailing: const Icon(Icons.more_vert),
+            ),
+            ListTile(
+              leading: Image.network(
+                  "https://www.khmersongs.net/data/upload/1546783868.jpg"),
+              title: const Text("Ly Sasa"),
+              subtitle: const Text("New Album 2022"),
+              trailing: const Icon(Icons.more_vert),
+            ),
+            ListTile(
+              leading: Image.network(
+                  "https://www.khmersongs.net/data/upload/1552311589.jpg"),
+              title: const Text("Ly Sasa"),
+              subtitle: const Text("New Album 2022"),
+              trailing: const Icon(Icons.more_vert),
             ),
           ],
         ),
