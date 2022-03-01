61fe950d761ab40e6da7ce14acc06d47bcbfc00e
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-01 15:39:20 +0700

    Update main.dart

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_app/page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,95 +12,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DefaultTabController(
-        length: 6,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              "Facebook",
-              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
-            ),
-            backgroundColor: Colors.white,
-            actions: const [
-              Icon(
-                Icons.search,
-                color: Colors.black,
-              ),
-              Icon(
-                Icons.chat,
-                color: Colors.black,
-              ),
-            ],
-            bottom: const TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(
-                    Icons.home,
-                    color: Colors.black,
-                  ),
-                ),
-                Tab(
-                  icon: Icon(
-                    Icons.tv,
-                    color: Colors.black,
-                  ),
-                ),
-                Tab(
-                  icon: Icon(
-                    Icons.shop,
-                    color: Colors.black,
-                  ),
-                ),
-                Tab(
-                  icon: Icon(
-                    Icons.flag,
-                    color: Colors.black,
-                  ),
-                ),
-                Tab(
-                  icon: Icon(
-                    Icons.notification_add,
-                    color: Colors.black,
-                  ),
-                ),
-                Tab(
-                  icon: Icon(
-                    Icons.list,
-                    color: Colors.black,
-                  ),
-                ),
-              ],
-            ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "facebook",
+            style: TextStyle(color: Colors.blue),
           ),
-          body: const TabBarView(
-            children: [
-              Icon(
-                Icons.home,
-                color: Colors.black,
-              ),
-              Icon(
-                Icons.tv,
-                color: Colors.black,
-              ),
-              Icon(
-                Icons.shop,
-                color: Colors.black,
-              ),
-              Icon(
-                Icons.flag,
-                color: Colors.black,
-              ),
-              Icon(
-                Icons.notification_add,
-                color: Colors.black,
-              ),
-              Icon(
-                Icons.list,
-                color: Colors.black,
-              ),
-            ],
-          ),
+          backgroundColor: Colors.white,
         ),
+        body: Home(),
       ),
     );
   }
