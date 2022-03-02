acd55d5a46abb868e0fe0a88d787c095ceec4777
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-02 23:55:28 +0700

    Update main.dart

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_app/page.dart';
+import 'package:my_first_app/HomePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
           ),
           backgroundColor: Colors.white,
         ),
-        body: const Home(),
+        body: HomePage(),
       ),
     );
   }
