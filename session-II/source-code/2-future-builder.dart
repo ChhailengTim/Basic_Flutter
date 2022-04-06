bd53a60c17f6f6b56da6b446b1d47195f987f195
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-04-06 23:04:13 +0700

    Create 2-future-builder.dart

---
+++ session-II/source-code/2-future-builder.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  Future<String> someFutureFunction(){
+    Future<String>data=Future.delayed(
+      Duration(seconds: 2),()=>"Latest News",
+    );
+    return data;
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: someFutureFunction(),
+        builder: (context,snapshot){
+          if(snapshot.connectionState==ConnectionState.done){
+            return Container(
+              child: Text(snapshot.data),
+            );
+          }
+          else{
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+    });
+  }
+}
+
