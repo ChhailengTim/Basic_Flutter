eb81cddc0bf2e7cb343864e6be8abfc247b12635
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-04-07 22:21:49 +0700

    api1 just add dependency and import package

---
+++ session-II/source-code/4-api1.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
